a783a3ba54c1224902a2a1c24f6ce34e32b71577
plin <zhi.chen@unity3d.com>
2022-09-28 11:01:41 +0800

    fix: android arm-v7a and arm64

--- BuildScripts~/build_plugin_android.sh
+++ BuildScripts~/build_plugin_android.sh
@@ -14,8 +14,10 @@ cd "$SOLUTION_DIR"
 
 for ARCH_ABI in armeabi-v7a arm64-v8a x86_64
 do
+  cd "$SOLUTION_DIR"
+  build_dir=build/$ARCH_ABI
   cmake . \
-    -B build \
+    -B $build_dir \
     -D CMAKE_SYSTEM_NAME=Android \
     -D CMAKE_ANDROID_API_MIN=24 \
     -D CMAKE_ANDROID_API=24 \
@@ -25,14 +27,31 @@ do
     -D CMAKE_ANDROID_STL_TYPE=c++_static
 
   cmake \
-    --build build \
-    --target WebRTCPlugin
+    --build $build_dir \
+    --target WebRTCPlugin || printf "\nlink failed but ignore\n"
 
-  echo $(pwd)
-  mkdir -p $PLUGIN_DIR/jni/$ARCH_ABI
-  mv $PLUGIN_DIR/libwebrtc.so $PLUGIN_DIR/jni/$ARCH_ABI
+  if [ -e "$PLUGIN_DIR/libwebrtc.so" ]; then
+    mkdir -p $PLUGIN_DIR/jni/$ARCH_ABI
+    mv $PLUGIN_DIR/libwebrtc.so $PLUGIN_DIR/jni/$ARCH_ABI/libwebrtc.so
+    echo "build success"
+  else
+    echo "build failed"
 
-  echo "\n----- Cmake build for $ARCH_ABI done -----\n\n"
+    cd "$SOLUTION_DIR/$build_dir/WebRTCPlugin"
+    printf "\n    $(pwd)\n"
+    cat ./CMakeFiles/WebRTCPlugin.dir/link.txt > ./CMakeFiles/WebRTCPlugin.dir/link.txt.bak
+    sed -i 's/--require-defined=[^[:space:]]* -Wl,//g' ./CMakeFiles/WebRTCPlugin.dir/link.txt
+
+    cmake -E cmake_link_script CMakeFiles/WebRTCPlugin.dir/link.txt --verbose=1 || echo "link failed but ignore--------------"
+
+    cd "$SOLUTION_DIR"
+
+    echo $(pwd)
+    mkdir -p $PLUGIN_DIR/jni/$ARCH_ABI
+    mv $PLUGIN_DIR/libwebrtc.so $PLUGIN_DIR/jni/$ARCH_ABI/libwebrtc.so || printf "$PLUGIN_DIR/libwebrtc.so not found\n"
+  fi
+
+  printf "\n----- Cmake build for $ARCH_ABI done -----\n\n"
 done
 
 
